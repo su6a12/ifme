c36f46b80d53fc4edc7052a04afe4dd8798eb5da
Suzan Esho <susan.esho@andela.com>
2016-11-13 19:45:28 +0100

    fix code climate issues

--- app/models/strategy_reminder.rb
+++ app/models/strategy_reminder.rb
@@ -1,7 +1,7 @@
 class StrategyReminder < ActiveRecord::Base
-	belongs_to :strategy
-	validates_inclusion_of :active, in: [true, false]
-	scope :active, -> { where(active: true) }
+  belongs_to :strategy
+  validates_inclusion_of :active, in: [true, false]
+  scope :active, -> { where(active: true) }
 
   def name
     I18n.t('strategies.daily_reminder')
--- app/services/perform_strategy_reminder.rb
+++ app/services/perform_strategy_reminder.rb
@@ -1,12 +1,12 @@
 class PerformStrategyReminders
   def send_startegy_reminder_emails
-    StrategyReminder.active.each do | reminder |
+    StrategyReminder.active.each do |reminder|
       NotificationMailer.perform_strategy(reminder).deliver_now
     end
   end
 
   def send_self_care_strategy_emails
-    ready_for_self_care.each do | reminder |
+    ready_for_self_care.each do |reminder|
       NotificationMailer.self_care_strategy(reminder).deliver_now
     end
   end
@@ -14,7 +14,10 @@ class PerformStrategyReminders
   private
 
   def ready_for_self_care
-    SelfCareStrategyReminder.active.joins(:strategy).where('strategies.self_care_strategy': one_week_from_now_as_string)
+    SelfcareReminder.active.joins(:strategy)
+                    .where(
+                  'strategies.self_care_strategy': one_week_from_now_as_string
+                          )
   end
 
   def one_week_from_now_as_string
